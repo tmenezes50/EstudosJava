c3c7f207afd76c5df437d2974baac7ceb1038130
hot <tmenezes50@gmail.com>
2022-07-18 17:06:16 -0300

    primeiro commit

---
+++ GUI/ShowColors2.java
@@ -0,0 +1,10 @@
+package GUI;
+import javax.swing.JFrame;
+
+public class ShowColors2 {
+    public static void main(String[] args) {
+        ShowColors2JFrame application = new ShowColors2JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
